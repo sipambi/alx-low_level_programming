4954ebc6d209731a1b32115e1ff5321a85d12d53
sipambi <sibandahmax@gmail.com>
2022-03-10 15:40:55 -0800

    4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*print needed sttring and returns 0*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
