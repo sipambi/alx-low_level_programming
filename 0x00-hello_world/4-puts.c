cb157f3611d1ef49a40cd125b89b67fe503719c8
sipambi <sibandahmax@gmail.com>
2022-03-10 15:34:18 -0800

    4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-gcc puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
