b17483f52dc9f1d9707574aff212bf4f43aa4596
sipambi <sibandahmax@gmail.com>
2022-03-11 15:47:55 -0800

    6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - writing numbers to base 10
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(char(number));
+	}
+	putchar('\n');
+	return (0);
+}
