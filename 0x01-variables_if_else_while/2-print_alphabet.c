2fce2791b08061ef648f9611fe5fd9cd4bfb85f6
sipambi <sibandahmax@gmail.com>
2022-03-11 15:15:01 -0800

    2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - checking for the last digit
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	char letter;
+	for(letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	return (0);
+}
