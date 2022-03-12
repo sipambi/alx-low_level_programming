88ec9941a6015811ebcf82ce21f26d35e0581a37
sipambi <sibandahmax@gmail.com>
2022-03-11 17:59:06 -0800

    10 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - combinations of three digits
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 1; b < 10; b++)
+		{
+			if (c = 2; c < 10: c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if(a != 7 || b != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+			secondNumber++;
+		}
+		firstNumber++;
+	}
+	putchar('\n');
+	return (0);
+}
