d1c72778ed97422f236727915279e63dd961a921
sipambi <sibandahmax@gmail.com>
2022-03-11 16:02:08 -0800

    7 commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - writing alphabet in reverse
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
