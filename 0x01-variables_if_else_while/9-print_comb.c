cec29bad266434af0ed2f4cce09f538035d93586
sipambi <sibandahmax@gmail.com>
2022-03-11 16:40:51 -0800

    9 commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 10; number ++)
+	for (number = 0; number < 10; number++)
 	{
 		putchar(number + 48);
-		if(number != 9)
+		if (number != 9)
 		{
 			putchar(',');
 			putchar(' ');
