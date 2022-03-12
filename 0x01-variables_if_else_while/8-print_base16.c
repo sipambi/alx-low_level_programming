c92d34c7e0f4d44d7a18c1957d38f58efc31e51f
sipambi <sibandahmax@gmail.com>
2022-03-11 16:13:52 -0800

    8 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - writing numbers in base 16
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	char letter;
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar(number + 48);
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
