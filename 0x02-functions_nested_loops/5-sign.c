e6dad3834a9a670e4aa149ecc87cdff13bd89366
sipambi <sibandahmax@gmail.com>
2022-03-15 11:44:41 -0700

    5 Commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ * @n : is the number to be checked
+ * Return: returns 1 and  prints + if n > 0, returns 0 if n == 0 and return -1 and prints - if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
