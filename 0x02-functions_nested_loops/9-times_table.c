ca532e078a434ee464aadae628bd273ef7400e2e
sipambi <sibandahmax@gmail.com>
2022-03-15 15:06:16 -0700

    9 Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 		for (column = 0; column <= 9; column++)
 		{
 			product = row * column;
-			tens = product / 10;;
+			tens = product / 10;
 			ones = product % 10;
 		
 			if (column == 0)
@@ -21,7 +21,7 @@ void times_table(void)
 			}
 			else if (product < 10)
 			{
-				_putchar('-');
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(ones + '0');
