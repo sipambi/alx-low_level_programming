aa8cd529f5a0d42a98f44f1a71b311109975845b
sipambi <sibandahmax@gmail.com>
2022-03-15 11:18:06 -0700

    3 Commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - checks for lowercse character
+ * Return: returns 1 if c is lowercase returns 0 otherwise
+ */
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
