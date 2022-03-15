6506438b25640968b1c4948e1fd848afe7cf7535
sipambi <sibandahmax@gmail.com>
2022-03-15 13:15:42 -0700

    8 Commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int mins;
+	while (1)
+	{
+		for (hour = 0; hour < 24; hour++)
+		{
+			for (mins = 0; mins < 60; mins++)
+			{
+				if (hour < 10)
+				{
+					_putchar('0');
+					_putchar(hour + 48);
+				}
+				else if (hour >= 10)
+				{
+					_putchar(hour + 48);
+				}
+				_putchar(':');
+				if (mins < 10)
+				{
+					_putchar('0');
+				}
+				_putchar(mins + 48);
+			}
+		}
+	}
+}
