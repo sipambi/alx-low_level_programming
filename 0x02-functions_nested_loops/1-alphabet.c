762750d0aa9abe091aff8c50331bef10cb85b32a
sipambi <sibandahmax@gmail.com>
2022-03-15 09:31:52 -0700

    1 Commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - print alphabet
+ * print_alphabet - print alphabet
  * Return: returns 0 if everything is okay
  */
 void print_alphabet(void)
 {
 	char i = 'a';
-	
+
 	while (i <= 'z')
 	{
 		_putchar(i);
