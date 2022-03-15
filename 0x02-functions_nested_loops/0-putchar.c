c19339efbe978b5040401c55e0be1dcc71d5477a
sipambi <sibandahmax@gmail.com>
2022-03-15 09:12:18 -0700

    0 Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[9] = "_putchar";
+	int i = 0;
+
+	while(i<8)
+	{
+		_putchar(text[i]);
+		i++;;
+	}
 	_putchar('\n');
 	return (0);
 }
