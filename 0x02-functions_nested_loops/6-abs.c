ce4291eabece521b36f3440507124e592ca36b9a
sipambi <sibandahmax@gmail.com>
2022-03-15 12:14:41 -0700

    6 Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute of a number
+ * @n : is the number to be checked
+ * Return: returns the absolute number
+ */
+
+int n;
+int _abs(n)
+{
+	if (n < 0)
+	{
+		n = n * 1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
