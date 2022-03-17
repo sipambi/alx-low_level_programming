8ffe3809915f1d467072ef6991d64a17985f0a8c
sipambi <sibandahmax@gmail.com>
2022-03-17 12:23:20 -0700

    0 Commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper() - checks for uppercase character
+ * _isupper - checks for uppercase character
  * @c : is the character to be checked
  * Return: 1 if c is uppercase and 0 otherwise
  */
