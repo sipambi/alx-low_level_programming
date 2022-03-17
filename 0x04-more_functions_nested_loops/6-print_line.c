f8fb0c5097d3998140184d7c27aa5505dbbf5f29
sipambi <sibandahmax@gmail.com>
2022-03-17 14:24:20 -0700

    6 Commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line in the terminal
+ * @n: variable for no. of lines
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int c = 0;
+
+	if (n > 0)
+	{
+		for (c = 0; c <= n; c++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
