80f1dd426b16c6c8fb352c2a417339ee7e5b95a0
sipambi <sibandahmax@gmail.com>
2022-03-17 13:38:17 -0700

    3 Commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers up to 9
+ * Return: 0 if successful
+ */
+
+void print_numbers(void)
+{
+	int c;
+	for(c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
