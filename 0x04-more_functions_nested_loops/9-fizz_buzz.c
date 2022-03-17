16709d78658fffa41671b940d88c2895950ff603
sipambi <sibandahmax@gmail.com>
2022-03-17 14:58:36 -0700

    9 Commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - print numbers up to 100
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if (c % 3 == 0 && c % 5 == 0)
+			printf("FizzBuzz ");
+		else if (c % 3 == 0)
+			printf("Fizz ");
+		else if (c % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d", c);
+	}
+	printf("Buzz\n");
+	return (0);
+}
