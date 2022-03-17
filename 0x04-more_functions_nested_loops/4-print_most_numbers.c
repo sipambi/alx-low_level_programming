3ec3f5faf1b52c69dd90981d7d8a25f49eb0a4c0
sipambi <sibandahmax@gmail.com>
2022-03-17 13:54:44 -0700

    4 Commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers up to 9
+ *
+ * Return: 0 void
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != '2' || c != '4')
+			_putchar(c);
+	}
+	_putchar('\n');
+}
