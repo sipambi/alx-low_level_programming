1670e92bb79af6ab80a9e5b6b95b65498b2802d4
sipambi <sibandahmax@gmail.com>
2022-03-21 15:09:59 -0700

    2 Commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strlen - returns the length off a string
@@ -9,5 +8,12 @@
 
 int _strlen(char *s)
 {
-	return (strlen(*s));
+	int length;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
 }
