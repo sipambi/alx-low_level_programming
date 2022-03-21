5edd6aa3c8f5a2c98383448925c47e261bb37f9a
sipambi <sibandahmax@gmail.com>
2022-03-21 15:01:51 -0700

    2 Commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length off a string
+ * @s: variable
+ * Returns: length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
