891f3557bfeacc82def015545e135d5a5ce46f7c
sipambi <sibandahmax@gmail.com>
2022-03-21 16:36:58 -0700

    6 Commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: variable
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
