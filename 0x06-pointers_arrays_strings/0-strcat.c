fcfbefb86984a3a320ec6e1459670cf9a6671bea
sipambi <sibandahmax@gmail.com>
2022-03-23 08:37:41 -0700

    0 Commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char result[100];
-	char *p = &result[];
-	int i, j = 0;
-
-	while(dest[i] != '\0')
+	int i = 0, j = 0;
+	
+	while (*(dest + i) != '\0')
 	{
-		result[j] = dest[i];
+		i++;
+	}
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
 		i++;
 		j++;
 	}
-	i = 0;
-	while(src[i] != '\0')
-	{
-		result[j] = src[i];
-		i++;
-		j++;
-	}
-
-	return p;
+	
+	return (dest);
 }
