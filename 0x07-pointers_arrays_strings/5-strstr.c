c09d77232d07cbe48b2853fbc96b36584e935979
sipambi <sibandahmax@gmail.com>
2022-03-28 12:28:21 -0700

    5 Commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @hastack: 1st variable
+ * @needle: 2nd variable
+ * Return: pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *hs = haystack, *n = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = n;
+		hs++;
+		haystack = hs;
+	}
+	return (0);
+}
