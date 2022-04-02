57cfe7bf588f936472b596cecab7d11b64459f96
sipambi <sibandahmax@gmail.com>
2022-04-02 01:11:11 -0700

    1 Commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints the number of arguments 
+ * @argc: first arg
+ * @argv: second arg
+ * Return 0: if no errors
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n" , argc);
+	return (0);
+}
