87f0fa62d6c80773e93ca61825acdec3ad61645e
sipambi <sibandahmax@gmail.com>
2022-04-02 01:16:34 -0700

    1 Commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - prints the number of arguments 
+ * main - prints the number of arguments
  * @argc: first arg
  * @argv: second arg
- * Return 0: if no errors
+ * Return: 0 if no errors
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n" , argc);
+	printf("%d\n", argc);
 	return (0);
 }
