cbbf52c1075f53d855f67ac93aad4c810b02d83c
sipambi <sibandahmax@gmail.com>
2022-04-02 08:16:53 -0700

    100 Commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - prints number of coins
+ * @argc: number of cmd line argu
+ * @argv: array of argu
+ * Return: 0 if no errors
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, ncoins = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
+	return (0);
+}
