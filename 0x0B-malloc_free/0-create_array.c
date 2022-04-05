7edac72c60eac7d93e094b9b6f9ad34e3470192b
sipambi <sibandahmax@gmail.com>
2022-04-05 14:01:18 -0700

    0 Commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - creates array
+ * @size: size of the data
+ * @c: char
+ * Return: a char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *cr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		cr[i] = c;
+
+	return (cr);
+}
