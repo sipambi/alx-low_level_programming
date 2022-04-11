8e211bded29cb666eff8730d176e639f9ea941d7
sipambi <sibandahmax@gmail.com>
2022-04-11 13:20:15 -0700

    0 Commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,19 @@
-typedef struct dog {
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: Struct is for man's best friend
+ */
+
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /*_DOG_*/
