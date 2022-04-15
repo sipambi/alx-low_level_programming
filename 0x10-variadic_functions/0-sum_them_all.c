4093165c36248687e8c33fcaf572770ec7475f40
sipambi <sibandahmax@gmail.com>
2022-04-15 14:58:39 -0700

    0 Commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_start(ap, n);
-	for (i 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 	var_end(ap);
 	return (sum);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 #ifndef _VARIADIC_
 #define _VARIADIC_
-#include "stdio.h"
-#include "stdarg.h"
+#include <stdio.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
