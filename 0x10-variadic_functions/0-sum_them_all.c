0101bdc72471c6431de815c5c57cf09575e89f4c
sipambi <sibandahmax@gmail.com>
2022-04-15 14:52:03 -0700

    0 Commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,11 +9,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int i, sum = 0;
+	unsigned int i;
+	int sum = 0;
 
-	va_start(ap, n);
 	if (n == 0)
 		return (0);
+	va_start(ap, n);
 	for (i 0; i < n; i++)
 		sum += va_arg(ap, int);
 	var_end(ap);
