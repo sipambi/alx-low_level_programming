4dbc51da0211291444699fbfd3241bb8a9d36060
sipambi <sibandahmax@gmail.com>
2022-04-27 12:52:52 -0700

    Linked Lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,8 +14,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *prev;
 	listint_t *next;
-	prev = *head;
 
+	prev = *head;
 	if (index != 0)
 	{
 		for (i = 0; i < index - 1 && prev != NULL; i++)
