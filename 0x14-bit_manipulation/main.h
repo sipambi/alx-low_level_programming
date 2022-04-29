9659b627866d0ff3450b794f5ce85d497f324187
sipambi <sibandahmax@gmail.com>
2022-04-29 12:26:09 -0700

    1 Commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef _BITS_
 #define _BITS_
-void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
