6620686a83abeabfd70ff9212943b198690fc25d
sipambi <96469754+sipambi@users.noreply.github.com>
2022-07-06 20:36:19 +0200

    0 Commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - create a hash table
- * @size: size of array
- * Return: new hash table or null if error
+ * hash_table_create - Create a hash table
+ * @size: size of the array
+ * Return: new hash table or NULL if error
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
