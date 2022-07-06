4a07debb5cdc61460b57e1ffbe43f7e3a2cd740b
sipambi <sibandahmax@gmail.com>
2022-07-06 11:20:09 -0700

    0 Commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: size of array
+ * Return: new hash table or null if error
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hsh;
+	unsigned long int i;
+
+	if(size == 0)
+		return (NULL);
+
+	hsh = malloc(sizeof(hash_table_t));
+	if (hsh == NULL)
+		return (NULL);
+
+	hsh->array = malloc(sizeof(hash_node_t *) * size);
+	if (hsh->array == NULL)
+	{
+		free(hsh);
+		return (NULL);
+	}
+	hsh->size = size;
+	
+	for (i = 0; i < size; i++)
+		hsh->array[i] = NULL;
+
+	return (hsh);
+}
