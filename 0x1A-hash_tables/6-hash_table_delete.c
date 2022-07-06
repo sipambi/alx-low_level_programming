b7a50dd81abf53601865dd6c716ab0b25da4f918
sipambi <sibandahmax@gmail.com>
2022-07-06 12:50:56 -0700

    extra

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,25 +4,28 @@
  * hash_table_delete - deletes a hash table.
  * @ht: hash table to be deleted.
  */
+
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *temp, *temp2;
-	unsigned long int i = 0;
+	unsigned long int i;
+	hash_table_t *head = ht;
+	hash_node_t *node, *temp;
 
-	if (ht == NULL)
-		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		temp = ht->array[i];
-		while (temp != NULL)
+		if (ht->array[i] != NULL)
 		{
-			temp2 = temp->next;
-			free(temp->key);
-			free(temp->value);
-			free(temp);
-			temp = temp2;
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
